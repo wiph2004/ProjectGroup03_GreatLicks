342b4597776dd924151839e1c2fcc79bdc5ab44a
Dakota.King <dakotaking92@gmail.com>
2024-05-05 20:25:44 -0500

    Update files and css on main

---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Cart from "../../components/Cart";
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div className="container">
+      <Cart />
+    </div>
+  );
+};
+
+export default Home;
