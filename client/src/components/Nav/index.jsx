11ae2e423c4ffb90620f12e0393600a696662659
Dakota.King <dakotaking92@gmail.com>
2024-05-08 19:02:45 -0500

    Make minor changes to dakota-branch

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -8,14 +8,14 @@ function Nav() {
   function showNavigation() {
     if (Auth.loggedIn()) {
       return (
-        <ul className="flex-row">
+       <ul className="flex-row">
           <li className="mx-1">
             <Link to="/orderHistory">
               Order History
             </Link>
           </li>
           <li className="mx-1">
-            {/* this is not using the Link component to logout or user and then refresh the application to the start */}
+             {/*this is not using the Link component to logout or user and then refresh the application to the start */}
             <a href="/" onClick={() => Auth.logout()}>
               Logout
             </a>
