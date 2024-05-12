179c2aec8eb89da26895ae428c8607f87d57b175
Dakota.King <dakotaking92@gmail.com>
2024-05-12 16:38:26 -0500

    Update landing page code

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 import './Home.css';
 import Cart from "../../components/Cart";
-import Shop from "../Shop/Shop";
+import bg from "../../assets/SnowBgTransparent.png"
 
 const Home = () => {
   return (
@@ -15,18 +14,19 @@ const Home = () => {
           Try our new strawberry ice cream sandwich on graham cracker cookies!
         </div>
       </div>
-            <Carousel width="80%" infiniteLoop="true" showThumbs={false}>
-                <div>
-                    <img src="../images/welcome.png" />
-                </div>
-                <div>
-                    <img src="../images/scoop.png" />
-                </div>
-                <div>
-                    <img src="../images/smile.png" />
-                </div>
-            </Carousel>
+      <Carousel width="80%" infiniteLoop="true" showThumbs={false}>
+          <div>
+              <img src="../images/welcome.png" />
           </div>
+          <div>
+              <img src="../images/scoop.png" />
+          </div>
+          <div>
+              <img src="../images/smile.png" />
+          </div>
+      </Carousel>
+      <img src={bg} alt="Landing" className="bg"/>
+    </div>
   );
 };
 
